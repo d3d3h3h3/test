867cb6f36070f41ff7ab4576536733a24ec8de89
Your Name <your-email@example.com>
2024-07-01 13:07:24 +0000

    Added Dockerfile and basic express server

---
+++ server.js
@@ -0,0 +1,5 @@
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 3000;
+app.get("/", (req, res) => res.send("Hello Climate Action Tracker"));
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
